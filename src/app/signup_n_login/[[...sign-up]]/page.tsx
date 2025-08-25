17e3848bce3f71708cff6e45a63f746946d20d46
tiper2066 <tiper2066@gmail.com>
2025-08-25 12:31:05 +0900

    custom login & signup make logic

--- src/app/signup_n_login/[[...sign-up]]/page.tsx
+++ src/app/signup_n_login/[[...sign-up]]/page.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import { useSignUp } from '@clerk/nextjs'; // ********************** Clerk useSignUp
-import { useRouter } from 'next/navigation'; // ********************** 라우터
+import { useSignIn, useSignUp } from '@clerk/nextjs'; //  Clerk useSignUp 와 useSignIn 추가
+import { useRouter } from 'next/navigation'; //  라우터
 import React, { useState } from 'react';
 
 const SignUpAndLogin = () => {
-    const { isLoaded, signUp, setActive } = useSignUp(); // ********************** Clerk hooks (로딩여부, 가입처리 함수, 활성여부) 활용
-    const [verify, setVerify] = useState(false); // ****************** 유효성 검증여부 상태 변수
-    const [code, setCode] = useState(''); // ********************** Clerk 데이터를 받아 확인할 경우 사용함
-    const router = useRouter(); // ************************ 라우터 객체 생성
+    const { signUp } = useSignUp(); //  Clerk hooks (로딩여부, 가입처리 함수, 활성여부) 활용,  isLoaded, setActive 는 중복되므로 여기서는 제거함
+    const { isLoaded, signIn, setActive } = useSignIn(); //  useSignIn 관련 메소드
+    const [verify, setVerify] = useState(false); //  유효성 검증여부 상태 변수
+    const [code, setCode] = useState(''); //  Clerk 데이터를 받아 확인할 경우 사용함
+    const router = useRouter(); //  라우터 객체 생성
 
     const [formType, setFormType] = useState('signup'); // login or signup 폼 구분 상태변수
     // SignUp 폼 요소값 상태변수
@@ -19,7 +20,7 @@ const SignUpAndLogin = () => {
     });
     // Login 폼 요소값 상태변수
     const [loginFields, setLoginFields] = useState({
-        identifire: '', // email (id 대신)
+        identifier: '', // email (id 대신)
         password: '',
     });
 
@@ -47,22 +48,38 @@ const SignUpAndLogin = () => {
         });
     };
 
+    // login or signup 폼 간의 애니메이션을 위한 함수
     const changeFormType = () => {
         if (formType === 'login') {
-            setFormType('signup');
+            setFormType('signup'); // 현재 login 타입이면.. signup 폼을 출력
         } else {
-            setFormType('login');
+            setFormType('login'); // 현재 signup 타입이면.. login 폼을 출력
         }
     };
 
-    const handleLoginSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    //  로그인 Submint 핸들러 함수 ( 비동기로 변경 )
+    const handleLoginSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (!isLoaded) return; // isLoaded가 false면 signIn이나 setActive 호출 방지
+        try {
+            const signInAttempt = await signIn?.create({ ...loginFields });
+
+            if (signInAttempt?.status === 'complete') {
+                await setActive({ session: signInAttempt.createdSessionId });
+                router.push('/dashboard');
+            } else {
+                console.error();
+            }
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    // **************************************************************** 회원 가입 Submit 핸들러 함수:
+    //  회원 가입 Submit 핸들러 함수:
     // verify 값에 따라서 검증 전이면 회원가입화면을 출력, 검증이 완료되면 전송 코드 입력화면을 출력함
     const handleSignUpSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        if (!isLoaded) return; //  setActive가 undefined일 가능성 방지
 
         try {
             await signUp?.create({ ...signUpFields }); // 폼필드 데이터값 이용하여 가입처리 함수 실행(이름, 이메일, 비번)
@@ -76,7 +93,7 @@ const SignUpAndLogin = () => {
         }
     };
 
-    // **************************************************************** 전송 코드 검증 핸들러 함수
+    //  전송 코드 검증 핸들러 함수
     const handleVerificationSubmit = async (
         e: React.FormEvent<HTMLFormElement>
     ) => {
@@ -92,8 +109,9 @@ const SignUpAndLogin = () => {
             // 코드 검증이 완료인 경우
             if (!isLoaded) return; // setActive가 undefined일 가능성 방지
             if (signupAttempt?.status === 'complete') {
-                await setActive({ session: signupAttempt.createdSessionId }); // 세션 id 생성함
-                router.push('/dashboard');
+                // await setActive({ session: signupAttempt.createdSessionId }); // 세션 id 생성함
+                // router.push('/dashboard');
+                setFormType('login'); //  회원가입 완료 후 로그인화면이 보이도록 한다
             } else {
                 console.error(signupAttempt);
             }
@@ -102,12 +120,12 @@ const SignUpAndLogin = () => {
         }
     };
 
-    if (!isLoaded) return null; // ************************************ Clerk loading 인되면 아무것도 반환하지 않움
+    if (!isLoaded) return null; //  Clerk loading 인되면 아무것도 반환하지 않움
 
     return (
         <div className='px-46 py-56 bg-gray-50 h-screen'>
             {/* ------------------------------ 로그인 UI ------------------------------ */}
-            <div className='hidden'>
+            <div className={formType === 'login' ? 'block' : 'hidden'}>
                 <div className='flex relative w-full min-w-[700px] max-w-[800px] h-[340px] space-y-5 rounded-lg border-1 border-gray-200 shadow-xl mx-auto'>
                     {/* --- Form 영역 --- */}
                     <form
@@ -171,7 +189,7 @@ const SignUpAndLogin = () => {
             </div>
 
             {/* ------------------------------ 회원 가입 UI ------------------------------ */}
-            <div>
+            <div className={formType === 'signup' ? 'block' : 'hidden'}>
                 <div className='flex relative w-full min-w-[700px] max-w-[800px] h-[340px] space-y-5 rounded-lg border-1 border-gray-200 shadow-xl mx-auto'>
                     {/* --- Sign title --- */}
                     <div className='flex items-center justify-center w-[50%] h-full py-10 px-5 rounded-lg bg-white'>
@@ -204,6 +222,7 @@ const SignUpAndLogin = () => {
                                     type='text'
                                     name='code'
                                     placeholder=''
+                                    value={code}
                                     onChange={(e) => setCode(e.target.value)} // 입력된 코드 갱신
                                     className='peer block w-full appearance-none rounded-lg border-1 border-gray-300 bg-white px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0'
                                 />
@@ -269,6 +288,11 @@ const SignUpAndLogin = () => {
                                     Password
                                 </label>
                             </div>
+                            {/*  Clerk CAPTCHA DOM */}
+                            <div
+                                id='clerk-captcha'
+                                style={{ marginTop: '1rem' }}
+                            ></div>
                             <button className='relative z-5 w-full mt-4 rounded-lg bg-blue-600 py-3 font-bold text-white hover:bg-blue-600/90 hover:cursor-pointer'>
                                 Submit
                             </button>
